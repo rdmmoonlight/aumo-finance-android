87d145e158aa6f8a5b2bcae556ee2f8adf838892
Obscuron <ndopoer@gmail.com>
2026-07-29 09:51:51 +0700

    Create DashboardModel.cs

---
+++ Models/DashboardModel.cs
@@ -0,0 +1,10 @@
+namespace AumoFinance.Models;
+
+public class DashboardModel
+{
+    public decimal TotalCash { get; set; }
+    public decimal Revenue { get; set; }
+    public decimal Expenses { get; set; }
+    public decimal NetIncome { get; set; }
+    public string ActivePeriod { get; set; } = string.Empty;
+}
