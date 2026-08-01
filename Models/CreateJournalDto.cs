a948db2d209916a8302347b225eda1487f32a666
Obscuron <ndopoer@gmail.com>
2026-08-01 14:37:58 +0700

    Refactor CreateJournalDto and add JSON property names

--- Models/CreateJournalDto.cs
+++ Models/CreateJournalDto.cs
@@ -1,15 +1,27 @@
+using System.Text.Json.Serialization;
+
 namespace AumoFinance.Models;
 
 public class CreateJournalDto
 {
-    public DateTime EntryDate { get; set; } = DateTime.UtcNow;
-    public string Description { get; set; } = string.Empty;
-    public List<JournalLineDto> Lines { get; set; } = new();
+    [JsonPropertyName("entryDate")]
+    public DateTime EntryDate { get; set; } = DateTime.Today;
+
+    [JsonPropertyName("lines")]
+    public List<CreateJournalLineDto> Lines { get; set; } = new();
 }
 
-public class JournalLineDto
+public class CreateJournalLineDto
 {
+    [JsonPropertyName("accountId")]
     public int AccountId { get; set; }
+
+    [JsonPropertyName("lineDescription")]
+    public string? LineDescription { get; set; }
+
+    [JsonPropertyName("debit")]
     public decimal Debit { get; set; }
+
+    [JsonPropertyName("credit")]
     public decimal Credit { get; set; }
 }
