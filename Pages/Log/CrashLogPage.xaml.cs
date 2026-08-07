f1eda49ac39c62ca745baa56eb4771a8a42ee3e8
Obscuron <ndopoer@gmail.com>
2026-08-07 14:52:50 +0700

    Add CrashLogPage for displaying and copying logs

---
+++ Pages/Log/CrashLogPage.xaml.cs
@@ -0,0 +1,27 @@
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.ApplicationModel;
+
+namespace AumoFinance.Pages.Log;
+
+public partial class CrashLogPage : ContentPage
+{
+    public CrashLogPage(string logContent)
+    {
+        InitializeComponent();
+        LogEditor.Text = logContent;
+    }
+
+    private async void OnCopyClicked(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrEmpty(LogEditor.Text))
+        {
+            await Clipboard.Default.SetTextAsync(LogEditor.Text);
+            await DisplayAlert("Sukses", "Seluruh teks log berhasil disalin ke clipboard!", "OK");
+        }
+    }
+
+    private async void OnCloseClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopModalAsync();
+    }
+}
