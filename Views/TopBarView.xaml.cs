90198ec990ecf57690983c96fb97e98a40ae505b
Obscuron <ndopoer@gmail.com>
2026-08-07 13:38:24 +0700

    Consolidate report page imports in TopBarView
    Removed individual report page imports and consolidated into a single import.

--- Views/TopBarView.xaml.cs
+++ Views/TopBarView.xaml.cs
@@ -4,17 +4,7 @@ using Microsoft.Maui.Controls;
 using AumoFinance.Pages;
 using AumoFinance.Pages.Coa;
 using AumoFinance.Pages.Periods;
-using AumoFinance.Pages.Reports.AdjustingJournal;
-using AumoFinance.Pages.Reports.ClosingJournal;
-using AumoFinance.Pages.Reports.GeneralJournal;
-using AumoFinance.Pages.Reports.GeneralLedger;
-using AumoFinance.Pages.Reports.IncomeStatement;
-using AumoFinance.Pages.Reports.PostClosingTrialBalance;
-using AumoFinance.Pages.Reports.RetainedEarnings;
-using AumoFinance.Pages.Reports.StatementOfCashFlows;
-using AumoFinance.Pages.Reports.StatementOfFinancialPosition;
-using AumoFinance.Pages.Reports.TrialBalance;
-using AumoFinance.Pages.Reports.Worksheet;
+using AumoFinance.Pages.Reports;
 
 namespace AumoFinance.Views;
 
