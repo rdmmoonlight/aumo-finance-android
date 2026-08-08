3fe5ebf6c0f58c87fff697cc4a2a2e5a11381794
Obscuron <ndopoer@gmail.com>
2026-08-08 11:36:51 +0700

    Implement OnCreate method for fingerprint initialization
    Added OnCreate method to initialize CrossFingerprint.

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,10 +1,22 @@
-﻿using Android.App;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.Fingerprint;
 
 namespace AumoFinance;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(
+    Theme = "@style/Maui.SplashTheme", 
+    MainLauncher = true, 
+    LaunchMode = LaunchMode.SingleTop, 
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        // Initialize CrossFingerprint with current activity
+        CrossFingerprint.SetCurrentActivityResolver(() => this);
+    }
 }
