c88e1dc1d4bcacc7c5b45255822a470df257a9c2
Obscuron <ndopoer@gmail.com>
2026-09-10 08:17:25 +0700

    del Maui

--- legacy-maui-reference/Models/AccountLookupDto.cs
+++
@@ -1,9 +0,0 @@
-namespace AumoFinance.Pages.JournalEntry;
-
-public class AccountLookupDto
-{
-    public int Id { get; set; }
-    public int ReferenceNumber { get; set; }
-    public string AccountName { get; set; } = string.Empty;
-    public string DisplayName { get; set; } = string.Empty;
-}
