b878af472b259a3028ee6166001e53a96c224196
Obscuron <ndopoer@gmail.com>
2026-09-17 14:22:02 +0700

    Add lifecycle-runtime-compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,5 +106,6 @@ dependencies {
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
